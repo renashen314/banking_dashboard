c372ba4f39af9155fa9cd1b6018be20cde512ce7
Rena Shen <renashen314@hotmail.com>
2024-11-06 12:10:05 -0500

    add doughnut chart with fixed data

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,12 +1,13 @@
 
 import React from 'react'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }: TotlaBalanceBoxProps) => {
   return (
     <section className='total-balance'>
         <div className='total-balance-chart'>
-            {/* donut chart */}
+            <DoughnutChart accounts={accounts} />
 
         </div>
         <div className='flex flex-col gap-6'>
