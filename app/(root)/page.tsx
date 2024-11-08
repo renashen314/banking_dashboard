8684b2360f4bb2cb7896122df8b13b2b39d53759
Rena Shen <renashen314@hotmail.com>
2024-11-08 16:16:41 -0500

    add mobile nav bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,29 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox'
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Dashboard = () => {
-    const loggedIn = { firstName: 'Rena' }
+  const loggedIn = { firstName: "Rena" };
   return (
-    <section className='home'>
-        <div className='home-content'>
-            <header className='home-header'>
-                <HeaderBox 
-                type='greeting'
-                title='Welcome'
-                user={loggedIn?.firstName || 'Guest'}
-                subtext="Access and manage your account and transactions efficiently."
-                />
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-                <TotalBalanceBox 
-                accounts={[]}
-                totalBanks={1}
-                totalCurrentBalance={1250.35}
-                />
-            </header>
-        </div>
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
