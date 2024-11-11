5194bf2840d812eab2ac7c174c6d499bcc41ea80
Rena Shen <renashen314@hotmail.com>
2024-11-10 19:42:15 -0500

    add loading state; add dynamic button text based on form type

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,9 +20,12 @@ import {
 import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof authFormSchema>>({
     resolver: zodResolver(authFormSchema),
@@ -38,7 +41,9 @@ const AuthForm = ({ type }: { type: string }) => {
   function onSubmit(values: z.infer<typeof authFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+    setIsLoading(true);
     console.log(values);
+    setIsLoading(false);
   }
 
   return (
@@ -69,39 +74,68 @@ const AuthForm = ({ type }: { type: string }) => {
       {user ? (
         <div className="flex flex-col gap-4">{/* {link plaid} */}</div>
       ) : (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <CustomInput
-              control={form.control}
-              name="email"
-              label="Email"
-              placeholder="Enter your email"
-              password={false}
-            />
-            <CustomInput
-              control={form.control}
-              name="username"
-              label="Username"
-              placeholder="Enter your username"
-              password={false}
-            />
-            <CustomInput
-              control={form.control}
-              name="password"
-              label="Password"
-              placeholder="Enter your password"
-              password={true}
-            />
-            <CustomInput
-              control={form.control}
-              name="confirmPassword"
-              label="Confirm Password"
-              placeholder="Confirm your password"
-              password={true}
-            />
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <CustomInput
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Enter your email"
+                password={false}
+              />
+              <CustomInput
+                control={form.control}
+                name="username"
+                label="Username"
+                placeholder="Enter your username"
+                password={false}
+              />
+              <CustomInput
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+                password={true}
+              />
+              <CustomInput
+                control={form.control}
+                name="confirmPassword"
+                label="Confirm Password"
+                placeholder="Confirm your password"
+                password={true}
+              />
+
+              <div className="flex flex-col gap-4">
+                <Button type="submit" disabled={isLoading} className="form-btn">
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </div>
+            </form>
+          </Form>
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Don't have a account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </footer>
+        </>
       )}
     </section>
   );
