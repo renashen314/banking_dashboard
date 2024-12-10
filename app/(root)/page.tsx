dd7e00d5cf49a5eb46b1a34fca9d7c11eb5cab13
Rena Shen <renashen314@hotmail.com>
2024-12-10 14:11:06 -0500

    add top categories ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,17 +6,16 @@ import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Dashboard = async ({ searchParams: {id, page}}:SearchParamProps) => {
-  const currentPage = Number(page as string) || 1
-  const loggedIn = await getLoggedInUser()
-  const accounts = await getAccounts({userId: loggedIn.$id})
+const Dashboard = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
 
-  if(!accounts) return;
+  if (!accounts) return;
 
-  const accountsData = accounts?.data
-  const appwriteItemId = (id as string) || accountsData[0].appwriteItemId
-  const account = await getAccount({ appwriteItemId })
-
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0].appwriteItemId;
+  const account = await getAccount({ appwriteItemId });
 
   return (
     <>
@@ -36,14 +35,18 @@ const Dashboard = async ({ searchParams: {id, page}}:SearchParamProps) => {
               totalCurrentBalance={accounts?.totalCurrentBalance}
             />
           </header>
-          <RecentTransactions 
-          accounts={accountsData}
-          transactions={account?.transactions}
-          appwriteItemId={appwriteItemId}
-          page={currentPage}
+          <RecentTransactions
+            accounts={accountsData}
+            transactions={account?.transactions}
+            appwriteItemId={appwriteItemId}
+            page={currentPage}
           />
         </div>
-        <RightSidebar user={loggedIn} transactions={accounts?.transactions} banks={accountsData.slice(0,2)} />
+        <RightSidebar
+          user={loggedIn}
+          transactions={account?.transactions}
+          banks={accountsData.slice(0, 2)}
+        />
       </section>
     </>
   );
